81cb2c866a8cca8be44467c1f9bc5e5d421d8807
Cristian <cristian.mila92@gmail.com>
2021-09-30 20:05:06 +0200

    asd

--- WebApplication1/Controllers/ValuesController.cs
+++ WebApplication1/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace WebApplication1.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "vsdasalue44441", "value2" };
+            return new string[] { "vsdasalueasd44441", "value2" };
         }
 
         // GET api/values/5
